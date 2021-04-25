325b80de5cf2628f3d6ca48af2253e16b0dd2935
MoranChery <chery@post.bgu.ac.il>
2021-04-25 21:04:10 +0300

    add menu

---
+++ src/components/Menu.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="menu">
+    <nav class="main-nav">
+      <img class="logo" src="../assets/logo.png">
+      <div class="logo"> LET'S IMPROVE </div>
+      <ul class="nav-bar">
+        <li class="item-nav" ><router-link to="/">דף הבית</router-link></li>
+        <li class="item-nav"><router-link to="/about">קצת עלינו</router-link></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Menu'
+}
+</script>
+<style>
+  .menu{
+    padding-left: 10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background: black;
+  }
+  .logo, .nav-bar{
+    text-align: center;
+    display: inline;
+    color: #fff;
+    font-weight: bold;
+    font-size: 30px;
+    border-radius: 100px;
+    width: 40px;
+    height: 40px;
+    vertical-align: middle;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+  .item-nav{
+    padding-right: 20px;
+    float: right;
+    display: block;
+  }
+
+  .item-nav a {
+    color: white;
+    text-align: center;
+    padding: 16px;
+    text-decoration: none;
+  }
+
+  .item-nav a:hover {
+    background-color: #117eb8;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,18 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Welcome to Let's Improve</h1>
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    // HelloWorld
+    HelloWorld
   }
 }
 </script>
