b40021133d2a82598b1ec31576c212c3dad2a703
MoranChery <chery@post.bgu.ac.il>
2021-10-08 11:34:27 +0300

    fixed home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home">
     <div class="filter">
-      <img class="logo-img" src="../assets/logo.png" alt="Logo">
-      <b-card title="ברוכים הבאים ל-Smart Save" style="max-width: 80rem;" class="mb-lg-0" img-alt="Image" img-top>
+      <b-card class="text-center position-relative" >
+        <b-img :src="require('../assets/logo.png')" height="350" width="350"></b-img>
+        <b-card-title> ברוכים הבאים ל-Smart Save</b-card-title>
         <b-card-text>אתר זה נוצר בכדי לאפשר השוואה בין מגוון קופות גמל להשקעה בישראל.</b-card-text>
         <b-card-text>ראיתי צורך לבנות אתר זה כדי לאפשר לאנשים להשואות קופות גמל באמצעות מחשבון פשוט והמלצות המערכת המבוססות על נתונים לפי בקשת המשתמש.</b-card-text>
         <b-card-text>תמיד חשבתי כיצד לחסוך את כספי בצורה חכמה ויעילה. הפחד שלי היה שבגלל חוסר ידע הייתי מאבד את הכסף שרציתי להשקיע בצורה חכמה.</b-card-text>
@@ -21,7 +22,6 @@ export default {
 <style scoped>
   .filter{
     direction: rtl;
-    height: 100%;
     text-align: justify;
     font-size: 18px;
   }
