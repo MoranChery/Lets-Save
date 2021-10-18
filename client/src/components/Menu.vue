f8de5d1fc8bf728b91ccc993ecfac8b0cab6b01c
MoranChery <chery@post.bgu.ac.il>
2021-10-18 17:27:55 +0300

    fixed Navbar to aligned screen size

--- client/src/components/Menu.vue
+++ client/src/components/Menu.vue
@@ -1,16 +1,27 @@
 <template>
-<div>
-  <b-navbar type="dark" variant="dark" >
+  <div>
+    <div>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
     <b-navbar-brand class="logo">
       <img class="logo-img" src="../assets/logo.png" alt="Logo">
       Smart Save
     </b-navbar-brand>
-    <b-navbar-nav class="navbar-nav ml-auto mb-2 mb-lg-0">
-      <b-nav-item to="/provident_fund_calculator">PPF Calculator</b-nav-item>
-      <b-nav-item to="/">דף הבית</b-nav-item>
-    </b-navbar-nav>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="מחשבונים" right>
+          <b-dropdown-item to="/provident_fund_calculator">קופת גמל</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-navbar-nav>
+          <b-nav-item to="/">דף הבית</b-nav-item>
+        </b-navbar-nav>
+      </b-navbar-nav>
+    </b-collapse>
   </b-navbar>
 </div>
+  </div>
+
 </template>
 <script>
 export default {
